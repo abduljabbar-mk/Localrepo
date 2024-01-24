321055182b5218098ba3fc50359cabe076a6fff0
Abdul Jabbar <abduljabbarme27@gmail.com>
2024-01-24 19:13:09 +0530

    Operators and condition code

---
+++ operators.py
@@ -0,0 +1,25 @@
+#Arthamatic operator
+# a = 5
+# b = 10
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# Relational Operator always return Boolean 
+
+#Conditional Statements
+#if if else elif
+
+#ex 1 : find number is even or odd
+
+num = int(input("Enter number to check: "))
+
+if num%2==0:
+    print("Number is even")
+else:
+    print("The number is odd")
