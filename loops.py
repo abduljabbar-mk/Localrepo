3c433542e235cac70b7f1164b6b186a2b7f8978b
Abdul Jabbar <abduljabbarme27@gmail.com>
2024-01-26 18:04:31 +0530

    New Loops File

---
+++ loops.py
@@ -0,0 +1,59 @@
+# print(list(range(10)))
+
+# #print odd numbers
+# print(list(range(1, 10, 2)))
+
+# #print Even Number
+# print(list(range(0, 10, 2)))
+
+# #print reverse even number
+# print(list(range(10, 0, -2)))
+
+# #print reverse odd number
+# print(list(range(10, 0, -1)))
+
+# i = 1
+
+# while i<=10:
+#     print(i)
+#     i=i+1
+# print("Done!!!")
+
+# i = 10
+
+# while i>=1:
+#     print(i)
+#     i=i-1
+# print("Done!!!")
+
+# for i in range(1, 10, 2):
+#     print(i)
+# print("Done!!!")
+
+
+# for i in range(2, 21, 2):
+#     print(i)
+
+# for i in range(20, 0, -2):
+#     print(i)
+
+#Break Contiue
+# for i in range(2, 21, 2):
+#     if i==10:
+#         break
+#     print(i)
+# print("Break excited!!!")
+
+# for i in range(2, 21, 2):
+#     if i == 10:
+#         continue
+#     print(i)
+# print("Continue:::")
+
+
+#Strings
+
+#mutable - Value can change & Immutable - Value Cannot be change
+
+str1="Welcome"
+print(id(str1))
