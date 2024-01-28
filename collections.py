b5edba815b43b56bae484cf3ff2b0ffe3396fd4f
Abdul Jabbar <abduljabbarme27@gmail.com>
2024-01-28 16:09:56 +0530

    Neww files

---
+++ collections.py
@@ -0,0 +1,10 @@
+myList=["apple", "Banana", "Orange"]
+
+# for i in myList:
+#     print(i)
+
+# myList.append("Mango")
+# print(myList)
+
+myList.insert(1, "Cherry")
+print(myList)
---
+++ stringSlicing.py
@@ -0,0 +1,21 @@
+# str="Welcome"
+# print(str * 5)
+
+
+#string Slicing
+# str= "My Name is Abdul"
+# print(str[11:])
+
+#ASCII number and Character (ord() and chr())
+# print(ord("A")) #65 is the ascii number
+# # print(chr(65)) #prints acsii character
+#method 1
+s= "welcome"
+# rev_s = ""
+# for i in s:
+#     rev_s=i+rev_s
+# print("reverse str:", rev_s)
+
+#method 2
+rev_s=s[::-1]
+print(rev_s)
